e395d9aaa2846c2eae74d3a9008ced9848711a09
Jagan Sunkavalli <sunkavallisathvik@gmail.com>
2025-09-16 21:12:25 +0530

    Initial commit

---
+++ src/main/java/net/jaymc/emeralddawn/EmeraldDawnDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jaymc.emeralddawn;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EmeraldDawnDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
