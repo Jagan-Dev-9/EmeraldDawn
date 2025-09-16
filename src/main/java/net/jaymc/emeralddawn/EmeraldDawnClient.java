56b15f229df3e25f2cb637549689d70529c74235
Jagan Sunkavalli <sunkavallisathvik@gmail.com>
2025-09-16 21:43:40 +0530

    Initial changes

---
+++ src/main/java/net/jaymc/emeralddawn/EmeraldDawnClient.java
@@ -0,0 +1,10 @@
+package net.jaymc.emeralddawn;
+
+import net.fabricmc.api.ClientModInitializer;
+public class EmeraldDawnClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
